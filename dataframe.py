aa755e394b9b382b4d8022650342f0469d24e651
NoExperienceCoder <nikunjsaini37@gmail.com>
2023-09-26 01:39:31 +0530

    merged data into one dataframe

--- dataframe.py
+++ dataframe.py
@@ -1,4 +1,16 @@
 import numpy as np
 import pandas as pd
 
-df1 = pd.read_excel('FinalSheet.xlsx')
+merged_list = []
+df1 = pd.DataFrame()
+
+sheet_name = ['2011-2015', '2012-2016','2013-2017', '2014-2018', '2015-2019', '2016-2020', '2017-2021', '2018-2022', '2019-2023']
+
+for i in range(len(sheet_name)):
+
+ 
+ merged_list.append(pd.read_excel('FinalSheet.xlsx', sheet_name[i]))
+
+df1 = pd.concat(merged_list, ignore_index=True)
+
+df1.to_csv('MergedList.csv')
