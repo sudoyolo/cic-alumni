b9ad1c2f6428b86f4808e3a1b9559edc68e20790
NoExperienceCoder <nikunjsaini37@gmail.com>
2023-10-11 00:27:47 +0530

    Added link to Login page and CIC home page.

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+   return render_template('index.html')
+
+@app.route('/SignIn')
+def SignIn_page():
+   return render_template('login.html')
+
+@app.route('/SearchAlumni')
+def Seacrh_alumni():
+   return render_template('index.html')
